29fe6f7a32d38855090c5065953f67209f09c342
Ben Brookes <brookesb91@hotmail.com>
2023-11-06 19:29:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const app = express();
 const port = 4000;
 
 app.use(express.static('public'));
-const whoseThat =
-  'https://e7.pngegg.com/pngimages/794/778/png-clipart-pokemon-quest-pokemon-sun-and-moon-pokemon-go-hitmonchan-hitmonchan-hand-monochrome-thumbnail.png';
 
 app.get('/', async (req, res) => {
   const team = [];
 
   for (let i = 0; i < 6; i++) {
     const species = await P.getPokemonSpeciesByName(
-      Math.floor(Math.random() * (1017 - 1) + 1)
+      Math.round(Math.random() * 1017)
     );
     team.push(species);
   }
