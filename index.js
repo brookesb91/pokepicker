a12d3869b4282f2ccac431aee285056e3552a37a
Ben Brookes <brookesb91@hotmail.com>
2023-11-06 15:15:31 +0000

    :wrench: Remove node_modules, updates

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-import express from "express";
+import express from 'express';
 import Pokedex from 'pokedex-promise-v2';
 const P = new Pokedex();
 const app = express();
 const port = 4000;
 
-app.use(express.static("public"));
-const whoseThat = "https://e7.pngegg.com/pngimages/794/778/png-clipart-pokemon-quest-pokemon-sun-and-moon-pokemon-go-hitmonchan-hitmonchan-hand-monochrome-thumbnail.png"
-app.get("/", (req, res) => {
-    res.render("index.ejs", {name: "?" , sprite: whoseThat});
-});
+app.use(express.static('public'));
+const whoseThat =
+  'https://e7.pngegg.com/pngimages/794/778/png-clipart-pokemon-quest-pokemon-sun-and-moon-pokemon-go-hitmonchan-hitmonchan-hand-monochrome-thumbnail.png';
 
-app.post("/pokemonOne", async (req, res) => {
-    try {
-        var randomNum = Math.floor(Math.random() * (1210 - 1) + 1);
-        var pokemonSpecies = await P.getPokemonSpeciesByName(randomNum)
-        var randomimage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + randomNum + ".png"
-    res.render("index.ejs", { 
-                 name: JSON.stringify(pokemonSpecies.name),
-                 sprite: randomimage
-            });
-    } 
-    catch (error) {
-        console.log(error);
-        }
+app.get('/', async (req, res) => {
+  const team = [];
+
+  for (let i = 0; i < 6; i++) {
+    const species = await P.getPokemonSpeciesByName(
+      Math.floor(Math.random() * (1017 - 1) + 1)
+    );
+    team.push(species);
+  }
+
+  res.render('index.ejs', { team });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
